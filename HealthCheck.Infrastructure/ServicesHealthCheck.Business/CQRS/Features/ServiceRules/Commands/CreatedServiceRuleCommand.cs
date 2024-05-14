b6455471eb3e422e90a2f34acac8ec39492425ce
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-05-14 13:48:44 +0300

    Service rule addition has been completed.

---
+++ HealthCheck.Infrastructure/ServicesHealthCheck.Business/CQRS/Features/ServiceRules/Commands/CreatedServiceRuleCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace ServicesHealthCheck.Business.CQRS.Features.ServiceRules.Commands
+{
+    public class CreatedServiceRuleCommand : IRequest
+    {
+        public string ServiceName { get; set; }
+        public string EventType { get; set; }
+        public string EventMessage { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ HealthCheck.Infrastructure/ServicesHealthCheck.Business/CQRS/Features/ServiceRules/Handlers/CommandHandlers/CreatedServiceRuleCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ServicesHealthCheck.Business.CQRS.Features.ServiceRules.Commands;
+using ServicesHealthCheck.DataAccess.Abstract;
+using ServicesHealthCheck.Datas.NoSQL.MongoDb;
+
+namespace ServicesHealthCheck.Business.CQRS.Features.ServiceRules.Handlers.CommandHandlers
+{
+    public class CreatedServiceRuleCommandHandler : IRequestHandler<CreatedServiceRuleCommand>
+    {
+        private readonly IServiceRuleRepository _serviceRuleRepository;
+        private readonly IMapper _mapper;
+        public CreatedServiceRuleCommandHandler(IMapper mapper, IServiceRuleRepository serviceRuleRepository)
+        {
+            _mapper = mapper;
+            _serviceRuleRepository = serviceRuleRepository;
+        }
+
+        public async Task Handle(CreatedServiceRuleCommand request, CancellationToken cancellationToken)
+        {
+            var serviceRule = _mapper.Map<ServiceRule>(request);
+            serviceRule.CreatedDate = DateTime.Now.AddHours(3);
+            await _serviceRuleRepository.AddAsync(serviceRule);
+        }
+    }
+}

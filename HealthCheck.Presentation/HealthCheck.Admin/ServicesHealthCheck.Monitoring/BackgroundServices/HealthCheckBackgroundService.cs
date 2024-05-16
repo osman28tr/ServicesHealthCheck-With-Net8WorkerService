bb68e2413c050efbfcced609ff95f081b9d79df6
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-05-16 23:14:20 +0300

    Eventviewer log connected to flag.

--- HealthCheck.Presentation/HealthCheck.Admin/ServicesHealthCheck.Monitoring/BackgroundServices/HealthCheckBackgroundService.cs
+++ HealthCheck.Presentation/HealthCheck.Admin/ServicesHealthCheck.Monitoring/BackgroundServices/HealthCheckBackgroundService.cs
@@ -42,24 +42,24 @@ namespace ServicesHealthCheck.Monitoring.BackgroundServices
 
                     // await _healthCheck.CheckServicesHealth(services);
                     var generalServiceHealthCheckDto = await _mediator.Send(new CreatedServiceHealthCheckCommand()
-                        { Services = services, ServiceResourceUsageLimit = serviceResourceUsageLimit });
+                    { Services = services, ServiceResourceUsageLimit = serviceResourceUsageLimit });
 
                     if (generalServiceHealthCheckDto.ServiceHealthCheckDtos.Any() || generalServiceHealthCheckDto.Errors.Any())
                     {
                         if (generalServiceHealthCheckDto.ServiceHealthCheckDtos.Any()) // güncellenecek servis varsa güncelle
                         {
                             var updatedServiceHealthCheckErrors = await _mediator.Send(new UpdatedServiceHealthCheckCommand()
-                                { ServiceHealthCheckDtos = generalServiceHealthCheckDto.ServiceHealthCheckDtos }); // servisleri güncelle, hata varsa al
+                            { ServiceHealthCheckDtos = generalServiceHealthCheckDto.ServiceHealthCheckDtos }); // servisleri güncelle, hata varsa al
                             if (updatedServiceHealthCheckErrors.Any()) // servisleri güncelleme sırasında hata varsa logla
                             {
                                 var updatedServiceErrorLogsByUpdateHealthCheck = await _mediator.Send(new CreatedServiceErrorLogCommand()
-                                    { Errors = updatedServiceHealthCheckErrors });
+                                { Errors = updatedServiceHealthCheckErrors });
                                 if (updatedServiceErrorLogsByUpdateHealthCheck.Any()) // loglanan hatalarda aynıları varsa güncelle
                                 {
                                     updatedServiceErrorLogsByUpdateHealthCheck.ForEach(async x =>
                                     {
                                         await _mediator.Send(new UpdatedServiceErrorLogCommand()
-                                            { Id = x.Id, IsCompleted = x.IsCompleted });
+                                        { Id = x.Id, IsCompleted = x.IsCompleted });
                                     });
                                 }
                             }
@@ -67,13 +67,13 @@ namespace ServicesHealthCheck.Monitoring.BackgroundServices
                         if (generalServiceHealthCheckDto.Errors.Any())
                         {
                             var updatedServiceErrorLogs = await _mediator.Send(new CreatedServiceErrorLogCommand()
-                                { Errors = generalServiceHealthCheckDto.Errors });
+                            { Errors = generalServiceHealthCheckDto.Errors });
                             if (updatedServiceErrorLogs.Any())
                             {
                                 updatedServiceErrorLogs.ForEach(async x =>
                                 {
                                     await _mediator.Send(new UpdatedServiceErrorLogCommand()
-                                        { Id = x.Id, IsCompleted = x.IsCompleted });
+                                    { Id = x.Id, IsCompleted = x.IsCompleted });
                                 });
                             }
                         }
@@ -82,11 +82,16 @@ namespace ServicesHealthCheck.Monitoring.BackgroundServices
                     //EventViewer Logs
                     Task.Run(async () =>
                     {
-                        var updateServiceRules = await _mediator.Send(new CreatedServiceEventViewerLogCommand
-                            { Services = services });
-                        if (updateServiceRules.Count != 0)
+                        var eventviewerLogRunState = configuration.GetSection("EventViewerLogRunState").Value;
+                        if (Convert.ToBoolean(eventviewerLogRunState) == true)
                         {
-                            await _mediator.Send(new UpdatedServiceRuleCommand { UpdateServiceRuleDtos = updateServiceRules });
+                            var updateServiceRules = await _mediator.Send(new CreatedServiceEventViewerLogCommand
+                            { Services = services });
+                            if (updateServiceRules.Count != 0)
+                            {
+                                await _mediator.Send(new UpdatedServiceRuleCommand
+                                { UpdateServiceRuleDtos = updateServiceRules });
+                            }
                         }
                     });
                 }
